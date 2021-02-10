8ce0397def41167d2a4ebe109ba11a505007042c
MattSymetry <xxmattiametryxx@gmail.com>
2021-02-10 16:14:09 +0100

    Revert "final db-migration script"
    This reverts commit c985acaf6c3ac870a60fc18016f9cce6b5663c2b.

--- database/migrations/2021_01_27_000000_create_wawi_tables.php
+++ database/migrations/2021_01_27_000000_create_wawi_tables.php
@@ -13,106 +13,31 @@ class CreateWawiTables extends Migration
      */
     public function up()
     {
-        Schema::create('user', function (Blueprint $table) {
-            $table->id('id');
+        Schema::table('user', function ($table) {
+            $table->id('user_id');
             $table->string('name');
             $table->string('username')->unique();
             $table->string('password');
-            //$table->remeberToken();
+           // $table->remeberToken();
             $table->timestamps();
         });
 
-        Schema::create('producer', function (Blueprint $table) {
-            $table->id('id');
-            $table->text('company');
-            $table->text('contact_firstname');
-            $table->text('contact_lastname');
-            $table->text('contact_phone');
-        });
-
-        Schema::create('product', function (Blueprint $table) {
-            $table->id('id');
+        Schema::table('product', function ($table) {
+            $table->id('product_id');
             $table->string('sku');
-            $table->boolean('is_live');
+            $table->bool('is_live');
             $table->string('title');
             $table->text('short_desc');
             $table->text('long_desc');
             $table->integer('courir_id');
             $table->integer('stock_count');
             $table->float('price');
-            $table->timestamps();
+            $table->datetime('created_at');
+            $table->datetime('updated_at');
             $table->string('calories');
             $table->string('sugar');
             $table->string('declaration');
-            $table->unsignedBigInteger('producer_id');
-            $table->unsignedBigInteger('category_id');
-            $table->foreign('producer_id')->references('id')->on('producer');
-        });
-
-        Schema::create('season', function (Blueprint $table) {
-            $table->id('id');
-            $table->text('season');
-            $table->date('date_from');
-            $table->date('date_to');
-        });
-
-        Schema::create('seasional_price', function (Blueprint $table) {
-            $table->id('id');
-            $table->float('season_price');
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('season_id');
-            $table->foreign('product_id')->references('id')->on('product');
-            $table->foreign('season_id')->references('id')->on('season');
-        });
-
-        Schema::create('courier', function (Blueprint $table) {
-            $table->id('id');
-            $table->text('firstname');
-            $table->text('lastname');
-            $table->text('phone');
-        });
-
-        Schema::create('delivery', function (Blueprint $table) {
-            $table->id('id');
-            $table->text('customer_firstname');
-            $table->text('customer_lastname');
-            $table->text('delivery_address');
-            $table->text('delivery_zip');
-            $table->text('delivery_place');
-            $table->text('delivery_country');
-            $table->unsignedBigInteger('courier_id');
-            $table->foreign('courier_id')->references('id')->on('courier');
-        });
-
-        Schema::create('delivery_product', function (Blueprint $table) {
-            $table->id('id');
-            $table->unsignedBigInteger('product_id');
-            $table->unsignedBigInteger('delivery_id');
-            $table->foreign('product_id')->references('id')->on('product');
-            $table->foreign('delivery_id')->references('id')->on('delivery');
-        });
-
-        Schema::create('category', function (Blueprint $table) {
-            $table->id('id');
-            $table->text('category');
-        });
-
-        Schema::create('special_price', function (Blueprint $table) {
-            $table->id('id');
-            $table->float('price');
-            $table->date('date_from');
-            $table->date('date_to');
-            $table->text('name');
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('product');
-        });
-
-        Schema::create('image', function (Blueprint $table) {
-            $table->id('id');
-            $table->integer('sort');
-            $table->text('path');
-            $table->unsignedBigInteger('product_id');
-            $table->foreign('product_id')->references('id')->on('product');
+            $table->integer('producer_id');
         });
     }
 
@@ -123,15 +48,6 @@ class CreateWawiTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('product');
-        Schema::dropIfExists('producter');
-        Schema::dropIfExists('user');
-        Schema::dropIfExists('category');
-        Schema::dropIfExists('seasional_price');
-        Schema::dropIfExists('season');
-        Schema::dropIfExists('special_price');
-        Schema::dropIfExists('delivery_product');
-        Schema::dropIfExists('delivery');
-        Schema::dropIfExists('courier');
+        Schema::dropIfExists('failed_jobs');
     }
 }
