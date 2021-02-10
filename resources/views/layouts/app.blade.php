953da9232e137375ef082b8bc1602097a923a017
benjamin <benjamin.antha.2001@gmail.com>
2021-02-10 09:29:57 +0100

    add navbar and changed sidebar height

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,22 +14,21 @@
           integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
 </head>
 <body>
-{{--<nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">--}}
-{{--    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">Sapium</a>--}}
-{{--    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">--}}
-{{--        <span class="navbar-toggler-icon"></span>--}}
-{{--    </button>--}}
-{{--    <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search">--}}
-{{--    <ul class="navbar-nav px-3">--}}
-{{--        <li class="nav-item text-nowrap">--}}
-{{--            <a class="nav-link" href="#">Sign out</a>--}}
-{{--        </li>--}}
-{{--    </ul>--}}
-{{--</nav>--}}
+<nav class="navbar navbar-dark sticky-top bg-primary flex-md-nowrap p-0 shadow">
+    <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">WAWI</a>
+    <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <ul class="navbar-nav px-3">
+        <li class="nav-item text-nowrap">
+            <a class="nav-link" href="#">Sign out</a>
+        </li>
+    </ul>
+</nav>
 
 <div class="container-fluid">
     <div class="row">
-        <nav id="sidebarMenu" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
+        <nav id="sidebarMenu" class="col-md-2 d-none d-md-block bg-light sidebar" style="height: 93vh;">
             <div class="sidebar-sticky pt-3">
                 <ul class="nav flex-column">
                     <li class="nav-item">
