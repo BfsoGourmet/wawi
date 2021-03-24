0b26c55158a1fda4f2090b062d5bb352b8bfe953
Luca Derivaz <derivaz.luca@gmail.com>
2021-03-24 14:46:19 +0100

    add - userManagement : role secretary

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -2,42 +2,43 @@
 
 @section('content')
 
-    @include('partials.errors')
+@include('partials.errors')
 
-    <h1>User erstellen</h1>
-    <form action="{{route('users.store')}}" method="POST">
-        @csrf
-        <div class="form-group">
-            <div class="col-sm-4">
-                <label for="name">{{__('Name')}}:</label>
-                <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-4">
-                <label for="email">{{__('E-Mail')}}:</label>
-                <input class="form-control" type="text" name="email" id="email" value="{{ old('email') }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-4">
-                <label for="password">{{__('Password')}}:</label>
-                <input class="form-control" type="text" name="password" id="password" value="{{ old('password') }}">
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-4">
-                <label for="role">{{__('Rolle')}}:</label>
-                <select class="form-control" type="text" name="role" id="role">
-                    <option value="guest">Guest</option>
-                    <option value="admin">Admin</option>
-                </select>
-            </div>
-        </div>
-        <div class="form-group">
-            <div class="col-sm-4 text-right">
-                <input type="submit" id="submit" value="Speichern" class="btn btn-primary">
-            </div>
-        </div>
-    </form>
+<h1>User erstellen</h1>
+<form action="{{route('users.store')}}" method="POST">
+  @csrf
+  <div class="form-group">
+    <div class="col-sm-4">
+      <label for="name">{{__('Name')}}:</label>
+      <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-4">
+      <label for="email">{{__('E-Mail')}}:</label>
+      <input class="form-control" type="text" name="email" id="email" value="{{ old('email') }}">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-4">
+      <label for="password">{{__('Password')}}:</label>
+      <input class="form-control" type="text" name="password" id="password" value="{{ old('password') }}">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-4">
+      <label for="role">{{__('Rolle')}}:</label>
+      <select class="form-control" type="text" name="role" id="role">
+        <option value="guest">Guest</option>
+        <option value="secretary">Secretary</option>
+        <option value="admin">Admin</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-4 text-right">
+      <input type="submit" id="submit" value="Speichern" class="btn btn-primary">
+    </div>
+  </div>
+</form>
 @stop
